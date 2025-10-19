b0f3e4a6b7e0319082e086626f7d9c7b2e109a10
Chamadi Weerasinghe <116907681+chamadi2002@users.noreply.github.com>
2025-10-19 21:45:09 +0530

    Update and rename database.php to database-example.php
    Updated database connection parameters for local development.

--- database-example.php
+++ database-example.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "sql109.infinityfree.com";
-$username = "if0_40150596";
-$password = "Xnhu1XfzEkLNd8Y";
-$dbname = "if0_40150596_grade_tracker";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "gradetracker";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,4 +11,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
